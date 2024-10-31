85f18262b20e052f7f4a411beed99db684b252f3
Danilo <damilhome@gmail.com>
2024-10-31 11:33:10 -0300

    limpando caixa de output para proxima conversao

--- script.js
+++ script.js
@@ -1,66 +1,46 @@
 const romanos = ['i', 'v', 'x', 'l', 'c', 'd', 'm'];
 const valores = [1, 5, 10, 50, 100, 500, 1000];
-const convertedNumber = [];
+let convertedNumber = [];
 const inputNumber = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 let userInput = null;
 
 function getRomanIndex(input) {
-    console.log('Entrou no pegar index')
     let index = 0;
 
     if(input > 1000) {
-        console.log('entrou no if do 1000')
         index = valores.length - 1;
     } else {
-        console.log('entrou no else do for')
         for (let i = 0; i < valores.length; i++) {
-            console.log('ciclo atual do for ' + i)
-            
             if(valores[i] === input) {
-                console.log('input é igual a valores ' + valores[i])
                 index = i;
                 break;
             } else if(valores[i] > input) {
-                console.log('input é menor que valores ' + valores[i - 1])
                 index = i - 1;
                 break;
             }
         }
     }
-    console.log(valores[index])
     return index;
-
 }
 
 function converter() {
-    console.log('entrou no converter')
     index = getRomanIndex(userInput)
-    console.log('saiu do pegar index')
     let ruleOfThree = true
 
     if(valores[index] * 3 < userInput) {
-        console.log('ENTROU NO IF PARA VERFICAR REGRA DO TRÊS')
         const newInput = userInput - valores[index] * 3;
-        ruleOfThree = newInput >= valores[index] ? false : true    
+        ruleOfThree = newInput >= valores[index] ? false : true;
     }
-    console.log('Regra do três: ' + ruleOfThree)
-    console.log('User input antes do if: ' + userInput)
 
     if(ruleOfThree) {
-        console.log('primeiro if converter')
-        console.log(valores[index])
         convertedNumber.push(romanos[index]);
         userInput -= valores[index];
-        console.log('User input: ' + userInput)
     } else {
-        console.log('segundo if converter')
-        console.log(valores[index + 1] - valores[index])
         convertedNumber.push(romanos[index]);
         convertedNumber.push(romanos[index + 1]);
         userInput -= (valores[index + 1] - valores[index]);
-        console.log('User input: ' + userInput)
     }
     
    if(userInput > 0) {
@@ -86,6 +66,7 @@ convertBtn.addEventListener('click', () => {
     } else {
         converter();
         output.classList.add('output__number');
-        output.textContent = convertedNumber.join('').toUpperCase();
+        output.innerText = convertedNumber.join('').toUpperCase();
+        convertedNumber = [];
     }
 })
