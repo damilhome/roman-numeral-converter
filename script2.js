dda343a3fcf1dea15d206fc924920567496ab328
Danilo <damilhome@gmail.com>
2024-10-31 22:34:56 -0300

    alerta se input estiver vazio e converter letras para maiuscula

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-const roman = ['i', 'v', 'x', 'l', 'c', 'd', 'm'];
+const roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
 const values = [1, 5, 10, 50, 100, 500, 1000];
 const minNumber = 1;
 const maxNumber = 3999;
@@ -6,6 +6,21 @@ const inputNumber = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 let decimal = 0;
+
+/* 
+TODO:
+- integrar esse script ao script inicial (de converter decimal para romano)
+- Converti todas as letras desse código para maiúscula, verificar se isso causa conflito
+na hora da integração com o outro script
+- com essa integração, toda vez que converter de romano para decimal,
+pegar esse decimal e chamar o script de conversão de decimal para romano
+e atualizar o valor que está sendo mostrado no input.value para esse novo romano,
+para garantir que o valor mostrado está escrito corretamente.
+- colocar um verificador para que, ao converter de romano para decimal, só chamar
+a outra conversão se o número for menor que 4000, caso contrário, mostrar a mensagem
+de alerta na tela
+- Veriificar se tem outros alertas para mostrar na tela
+*/
 
 function convertToDecimal(userInput) {
     console.log(`Started the conversion function`)
@@ -49,14 +64,20 @@ function startConvert() {
     console.log(`Start to convert`)
     const userInput = inputNumber.value;
 
-    convertToDecimal(userInput);
-    console.log(`Ended conversion function`)
-    output.classList.add('output__number');
-    output.textContent = decimal;
-    decimal = 0;
+    if(userInput === '') {
+        output.classList.remove('output__number');
+        output.classList.add('output__alert');
+        output.textContent = 'Please enter a valid number';
+    } else {
+        convertToDecimal(userInput);
+        console.log(`Ended conversion function`)
+        output.classList.remove('output__alert');
+        output.classList.add('output__number');
+        output.textContent = decimal;
+        decimal = 0;
+    }
 }
-
-inputNumber.addEventListener('keydown', (e) => {
+function checkInvalidLetter(e) {
     let invalidLetter = true;
 
     roman.forEach(letter => {
@@ -68,7 +89,15 @@ inputNumber.addEventListener('keydown', (e) => {
     if(invalidLetter) {
         e.preventDefault();
     }
-})
+}
+
+function letterToUpperCase(e) {
+    e.target.value = e.target.value.toUpperCase();
+}
+
+
+inputNumber.addEventListener('keydown', (e) => checkInvalidLetter(e));
+inputNumber.addEventListener('input', (e) => letterToUpperCase(e));
 
 convertBtn.addEventListener('click', startConvert)
 inputNumber.addEventListener('keydown', (e) => {
